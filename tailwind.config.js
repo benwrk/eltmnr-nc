245d1ff2f4ee733966509bf84880af3cea55eb12
Benjapol Worakan <benwrk@live.com>
2025-01-20 10:12:02 +0700

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./app/**/*.{js,vue,ts}', './content/**/*.{js,vue,ts}'],
+  theme: {
+    extend: {
+      colors: {}
+    }
+  },
+  plugins: []
+}
